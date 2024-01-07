89b3a7d2294f8c33421d5ec66c2abfdcab5a6841
jsbudge <penryn101@gmail.com>
2024-01-07 13:09:20 -0700

    Added some resource bases for orbs, equipment, and items.

---
+++ assets/items/accessories/equip_base.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name Equipment
+
+@export var name: String
+@export var texture: Texture2D
+@export var description: String
+@export var harmonics: Array[int]
+@export var stat_modifiers: Array[int]
+@export var slot: int
---
+++ assets/items/orbs/orb_base.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name Orb
+
+@export var name: String
+@export var texture: Texture2D
+@export var description: String
+@export var harmonics: Array[int]
+@export var stat_modifiers: Array[int]
+@export var move_types: Array[int]
+@export var moves: Array
