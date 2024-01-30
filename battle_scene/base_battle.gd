17b1ff9847b4d9ee9b62739c42e7e9073ccace69
jbudge <jbudge@artemisinc.net>
2024-01-30 15:47:03 -0700

    Added a resource spreadsheet plugin from AssetLib. Also working on menu changes and selection for battle scene. Added a bunch of moves to test move selection.

--- battle_scene/base_battle.gd
+++ battle_scene/base_battle.gd
@@ -3,6 +3,9 @@ extends Node2D
 const BattleTile = preload("res://battle_scene/battle_tile.tscn")
 const BattleChar = preload("res://battle_scene/battle_character.tscn")
 @onready var grid: Array[Array] = [[], [], [], [], [], []]
+@onready var players: Array
+var mode = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for x in range(5):
@@ -11,13 +14,35 @@ func _ready():
 			btile.position = Vector2(100 + x * 50, 150 + y * 50)
 			grid[x].append(btile)
 			add_child(btile)
-	load_player(100, 100, ActivePlayer.player_data.stats, ActivePlayer.player_data.harmonics, 1, "Test Dude", 0, grid[2][1].position)
+	load_player(100, 100, ActivePlayer.player_data.stats, ActivePlayer.player_data.harmonics, 
+	1, "Test Dude", 0, grid[2][1].position + Vector2(16, 16))
+	$Select.visible = false
+	$Select.stop()
 			
 func load_grid(grid_data):
 	pass
+	
+func _process(delta):
+	if mode == 1:
+		if Input.is_action_just_pressed("move_down"):
+			$Select.position = players[0].position + Vector2(0, 50)
+		elif Input.is_action_just_pressed("move_up"):
+			$Select.position = players[0].position - Vector2(0, 50)
+		elif Input.is_action_just_pressed("move_left"):
+			$Select.position = players[0].position + Vector2(50, 0)
+		elif Input.is_action_just_pressed("move_right"):
+			$Select.position = players[0].position - Vector2(50, 0)
 	
 func load_player(max_hp, curr_hp, stats, curr_harmonics, curr_status, curr_name, curr_team, ppos):
 	var player = BattleChar.instantiate()
 	player.set_values(max_hp, curr_hp, stats, curr_harmonics, curr_status, curr_name, curr_team)
 	player.position = ppos
+	players.append(player)
 	add_child(player)
+
+
+func _on_battle_ui_atk_select(atk_num):
+	$Select.position = players[0].position + Vector2(50, 0)
+	$Select.visible = true
+	$Select.play()
+	mode = 1
--- battle_scene/battle_ui.gd
+++ battle_scene/battle_ui.gd
@@ -1,21 +1,29 @@
 extends Control
 
+signal atk_select
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var aorb = ActivePlayer.getActiveOrb()
 	$SwapGrid.visible = false
 	$AttackGrid.visible = false
 	$OptionGrid.visible = true
-	$ActiveOrbDisplay.texture = ActivePlayer.getActiveOrb().texture
+	$ActiveOrbDisplay.texture = aorb.texture
 	var idx = 0
 	for child in $SwapGrid.get_children():
 		child.text = ActivePlayer.player_data.ready_orbs[idx].name
 		idx += 1
-	'''
 	idx = 0
 	for child in $AttackGrid.get_children():
-		child.text = ActivePlayer.getActiveOrb().moves[idx]
-		idx += 1'''
+		if aorb.moves[idx]:
+			child.text = aorb.moves[idx].name
+			child.tooltip_text = aorb.moves[idx].description
+		else:
+			child.visible = false
+		idx += 1
+		
+func writeInfo(s: String) -> void:
+	$InfoPanel/InfoLabel.text = s
 
 
 func _on_swap_button_pressed():
@@ -27,3 +35,18 @@ func _on_swap_button_pressed():
 func _on_swap_orb_pressed(butt_num: int):
 	ActivePlayer.setActiveOrb(butt_num)
 	_ready()
+
+
+func _on_attack_button_pressed():
+	$OptionGrid.visible = false
+	$AttackGrid.visible = true
+	# $AttackGrid/Atk1.grab_focus()
+
+
+func _on_button_focus_entered(message: String):
+	writeInfo(message)
+
+
+func _on_atk_pressed(butt_num: int) -> void:
+	$Select.visible = true
+	$Select/AnimatedSprite2D.play()
